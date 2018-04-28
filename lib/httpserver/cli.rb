ab7ca0d379a99f31983f52efeeff60dc6ccd656e
Yuka Matsuo <matsuo.yk@gmail.com>
2018-04-28 16:17:33 +0900

    listen部分をメソッド分割

--- lib/httpserver/cli.rb
+++ lib/httpserver/cli.rb
@@ -5,10 +5,16 @@ module Httpserver
   class CLI < Thor
     option(:host, :type => :string, :default => "0.0.0.0")
     option(:port, :type => :numeric, :default => 8080)
-    desc("listen [host] [port]", "")
+    desc("echo [host] [port]", "up echo server")
     def echo
-      server = Server.new
-      server.echo(options[:host], options[:port])
+      Server.new(options[:host], options[:port]).echo
+    end
+
+    option(:host, :type => :string, :default => "0.0.0.0")
+    option(:port, :type => :numeric, :default => 8080)
+    desc("echo_multi_thread [host] [port]", "up multi thread echo server")
+    def echo_multi_thread
+      Server.new(options[:host], options[:port]).echo_multi_thread
     end
   end
 end
--- lib/httpserver/server.rb
+++ lib/httpserver/server.rb
@@ -3,13 +3,42 @@ require 'socket'
 
 module Httpserver
   class Server
-    def echo(host, port)
-      Socket.tcp_server_loop(host, port) { |client, address|
+    def initialize(host, port)
+      @host = host
+      @port = port
+    end
+
+    def listen
+      Socket.tcp_server_loop(@host, @port) { |client, address|
         begin
-          IO.copy_stream(client, client)
+          yield client, address
         ensure
           client.close
         end
+      }
+    end
+
+    def listen_multi_thread
+      Socket.tcp_server_loop(@host, @port) { |client, address|
+        Thread.new {
+          begin
+            yield client, address
+          ensure
+            client.close
+          end
+        }
+      }
+    end
+
+    def echo
+      listen { |client, address|
+        IO.copy_stream(client, client)
+      }
+    end
+
+    def echo_multi_thread
+      listen_multi_thread { |client, address|
+        IO.copy_stream(client, client)
       }
     end
   end
